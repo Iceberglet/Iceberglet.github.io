6755b1e40e5f2b9c1d7533fc2ce32b5ec6e9ab87
Iceberglet <dragoon.cm@hotmail.com>
2017-02-21 23:45:17 +0800

    First Splash Screen. More Languages and Colors to Be Added

--- source/index.js
+++ source/index.js
@@ -2,6 +2,9 @@ import { App } from './app';
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './css/base.css'
+import './css/splash.css'
+
 let div = document.createElement('div');
 document.body.appendChild(div);
 ReactDOM.render(
