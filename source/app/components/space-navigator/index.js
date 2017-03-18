ed5bbbfef9b7533c5c2d5883b8e056fa7c3fa46c
Iceberglet <dragoon.cm@hotmail.com>
2017-03-18 18:28:04 +0800

    basic parallax

--- source/app/components/space-navigator/index.js
+++ source/app/components/space-navigator/index.js
@@ -28,14 +28,27 @@ export const SpaceNavigator = React.createClass({
         })
       })
     })
-
-    //For parallax items
-    // let scopeEl = ReactDOM.findDOMNode(this.refs.scope)
-    // this.f = registerToMouse((x, y)=>{
-    //   scopeEl.style.top = y+'px'
-    //   scopeEl.style.left = x+'px'
-    // })
+    this.subscribeToParallax(1, this.refs.backdrop)
+    this.subscribeToParallax(5, this.refs.overlay)
   },
+
+  subscribeToParallax(amount, target){
+    let el = ReactDOM.findDOMNode(target)
+    if(el){
+      this.unsubscribeParallax(el);
+      ((e)=>{
+        registerToMouse((x, y)=>{
+          let leftPerc = x / window.innerWidth
+          e.style.left = (leftPerc - 0.5) * amount * 10 + 'px'
+          let topPerc = y / window.innerHeight
+          e.style.top = (topPerc - 0.5) * amount * 10 / window.innerWidth * window.innerHeight + 'px'
+        })
+      })(el)
+    }
+  },
+  unsubscribeParallax(element){
+  },
+
   componentWillUnmount(){
     delete this.canvas
   },
@@ -52,11 +65,11 @@ export const SpaceNavigator = React.createClass({
   render(){
     this.targetable = []
     return (<div className='space-navigator' onMouseEnter={this.mouseIn} onMouseLeave={this.mouseLeave} >
-          <div className='background' ref='slowest'>
+          <div className='background' ref='backdrop'>
             <img src={'resources/space2.jpg'}/>
           </div>
-          <div className='overlay'>
-            {this.state.mouseIn && <Scope ref='cursor' targetNode={this.state.targetNode}/>}
+          <div className='overlay' ref='overlay'>
+            {this.state.mouseIn && <Scope ref='middle' targetNode={this.state.targetNode}/>}
             <div className='test-box' ref={x=>{this.targetable.push(x)}} />
             <div className='test-box' ref={x=>{this.targetable.push(x)}} />
             <div className='test-box' ref={x=>{this.targetable.push(x)}} />
