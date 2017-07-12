5605bed26fe372821980e8e94095b7afbee5faf7
Iceberglet <min.chen@finmechanics.com>
2017-07-12 21:53:40 +0800

    Place Back Fancy-Tab-Panel

--- source/app/pages/page-gallery/item-tab-panel.js
+++ source/app/pages/page-gallery/item-tab-panel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import ReactFancyTabPanel from 'fancy-tab-panel'
-import ReactFancyTabPanel from 'react-fancy-tab-panel'
+import ReactFancyTabPanel from 'fancy-tab-panel'
 
 const TabPanelExample = ReactFancyTabPanel.TabPanelExample
 
