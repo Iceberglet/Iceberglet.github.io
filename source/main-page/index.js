10cea5797dcf60176b56fecca200ff6204927987
Iceberglet <min.chen@finmechanics.com>
2017-08-02 22:17:17 +0800

    mobile browser detection

--- source/main-page/index.js
+++ source/main-page/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Splash from './splash'
 import {GridContainer} from 'animated-grid'
 import {GridData} from './grid-data'
+import {isMobile} from 'utils'
 
 import './main-page.scss'
 
@@ -18,7 +19,8 @@ export default class MainPage extends React.Component {
   }
 
   render(){
-    return <div className='main-page'>
+    let style = isMobile? {} : {minWidth: '600px', minHeight: '320px'}
+    return <div className='main-page' style={style}>
       <div className='central'>
         <Splash onClickAvatar={this.onTriggerShow}/>
         <div className='main-page-grid'>
